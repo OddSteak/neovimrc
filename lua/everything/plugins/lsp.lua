3f3598ab8da293691a2cdf2a0ec1d5320d4c51a8
OddSteak <sedentaryowl@riseup.net>
2024-06-15 03:42:20 +0800

    minor package fixes
    folding, luasnip jsregexp, css lsp, fixed deprecated function in harpoon

--- lua/everything/plugins/lsp.lua
+++ lua/everything/plugins/lsp.lua
@@ -26,6 +26,7 @@ return {
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
+                "cssls",
                 "lua_ls",
                 "pyright",
                 "bashls",
